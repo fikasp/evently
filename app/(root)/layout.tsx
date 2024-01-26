6f5f153a6a54a484d41893fd95c3ac945814c371
fikasp <fikasp@gmail.com>
2024-01-26 03:27:40 +0100

    Add layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/shared/Footer"
-import Header from "@/components/shared/Header"
+import Footer from "@/components/layout/Footer"
+import Header from "@/components/layout/Header"
 
 export default function RootLayout({
   children,
