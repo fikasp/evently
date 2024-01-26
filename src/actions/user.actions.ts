355e3d61d648054ab12e010ce29370d1ed4ccf2b
fikasp <fikasp@gmail.com>
2024-01-26 02:51:13 +0100

    Fix paths

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -2,13 +2,13 @@
 
 import { revalidatePath } from 'next/cache'
 
-import { connectToDatabase } from '@/lib/database'
-import User from '@/lib/database/models/user.model'
-import Order from '@/lib/database/models/order.model'
-import Event from '@/lib/database/models/event.model'
-import { handleError } from '@/lib/utils'
+import { connectToDatabase } from '@/database'
+import User from '@/database/models/user.model'
+import Order from '@/database/models/order.model'
+import Event from '@/database/models/event.model'
+import { handleError } from '@/utils'
 
-import { CreateUserParams, UpdateUserParams } from '@/src/types'
+import { CreateUserParams, UpdateUserParams } from '@/types'
 
 export async function createUser(user: CreateUserParams) {
   try {
