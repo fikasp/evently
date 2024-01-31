9323614eebb4ce82e699128955d023b38cd45eb6
fikasp <fikasp@gmail.com>
2024-01-31 01:32:28 +0100

    Revert profile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -22,29 +22,6 @@ export default async function ProfilePage({ searchParams }: SearchParamProps) {
 
 	return (
 		<>
-			{/* My Tickets */}
-			<section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-				<div className="wrapper flex items-center justify-center sm:justify-between">
-					<h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
-					<Button asChild size="lg" className="button hidden sm:flex">
-						<Link href="/#events">Explore More Events</Link>
-					</Button>
-				</div>
-			</section>
-
-			<section className="wrapper my-8">
-				<Collection
-					data={orderedEvents}
-					emptyTitle="No event tickets purchased yet"
-					emptyStateSubtext="No worries - plenty of exciting events to explore!"
-					collectionType="My_Tickets"
-					limit={3}
-					page={ordersPage}
-					urlParamName="ordersPage"
-					totalPages={orders?.totalPages}
-				/>
-			</section>
-
 			{/* Events Organized */}
 			<section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
 				<div className="wrapper flex items-center justify-center sm:justify-between">
@@ -65,6 +42,29 @@ export default async function ProfilePage({ searchParams }: SearchParamProps) {
 					page={eventsPage}
 					urlParamName="eventsPage"
 					totalPages={organizedEvents?.totalPages}
+				/>
+			</section>
+			
+			{/* My Tickets */}
+			<section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+				<div className="wrapper flex items-center justify-center sm:justify-between">
+					<h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+					<Button asChild size="lg" className="button hidden sm:flex">
+						<Link href="/#events">Explore More Events</Link>
+					</Button>
+				</div>
+			</section>
+
+			<section className="wrapper my-8">
+				<Collection
+					data={orderedEvents}
+					emptyTitle="No event tickets purchased yet"
+					emptyStateSubtext="No worries - plenty of exciting events to explore!"
+					collectionType="My_Tickets"
+					limit={3}
+					page={ordersPage}
+					urlParamName="ordersPage"
+					totalPages={orders?.totalPages}
 				/>
 			</section>
 		</>
